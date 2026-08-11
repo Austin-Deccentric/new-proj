340c1b6b8766b24b12c00f740396d2805ab96be9
CletsyMedia <cletussam12@yahoo.com>
2026-08-11 21:40:49 +0100

    ✨ tanstack 📅 11-08-2026 ⏳ 21:40:48

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
+module.exports = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "cdn.dummyjson.com" }],
+  },
+};
 
 export default nextConfig;
